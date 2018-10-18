2f1b4430eb79409d6c22a40bdacbd5c52c3864c4
Hardy <hardiansyah.iiI@gmail.com>
2018-10-18 20:27:50 +0700

    add echo commands to bootstrap to show progress -- need to put an if before every action to not overlap when installing required items

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,61 +11,54 @@ PINGIT_REPO=/webapps/repo/pingit
 # TODO: move dependencies into text file
 sudo apt-add-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt-get install -y python3.6
-sudo apt-get install -y python-virtualenv libpq-dev python-dev 
-sudo apt-get install -y supervisor nginx
-sudo apt-get install -y postgresql postgresql-contrib
+echo "installing python3.6"
+sudo apt-get install -y python3.6 && echo "installed python3.6"
+echo "installing python dependencies"
+sudo apt-get install -y python-virtualenv libpq-dev python-dev && echo "installed python dependencies"
+echo "installing supervisor and nginx"
+sudo apt-get install -y supervisor nginx && echo "installed supervisor and nginx"
+echo "installing postgres"
+sudo apt-get install -y postgresql postgresql-contrib && echo "installed postgres"
 
 # create user to run the project // use users to run applications safely by implementing proper priviliges
-sudo groupadd --system webapps
-sudo useradd --system --gid webapps --shell /bin/bash --home $PROJECT_DIR pingit
+sudo mkdir -p $PROJECT_DIR 
+sudo groupadd --system webapps && echo "created webapps group"
+sudo useradd --system --gid webapps --shell /bin/bash --home $PROJECT_DIR pingit && echo "created pingit user"
+sudo chown pingit:webapps $PROJECT_DIR && echo "set permissions"
 
 # create role and db in psql
-sudo -su postgres createuser boong
-sudo -su postgres createdb boong_bank --owner=boong
+sudo -su postgres createuser boong && echo "create psql role boong"
+sudo -su postgres createdb boong_bank --owner=boong && echo "create psql database boong_bank"
 
 # work-tree directory setup
-sudo mkdir -p $PROJECT_DIR
-sudo chown pingit:webapps $PROJECT_DIR
-## log folder for NGINX and supervisor
-cd $PROJECT_DIR
-mkdir logs
-touch logs/gunicorn_supervisor.log
 ## clone repo and remove unnecessary files and dirs
+echo "cloning repo"
 git clone $PROJECT_GITHUB_REPO . &&
 rm -rf .git &&
 rm .gitignore &&
 rm -rf .travis &&
-rm .travis.yml
+rm .travis.yml && echo "repo cloned and unnecessary files removed"
+
+## log folder for NGINX and supervisor
+echo "creating log dir and files"
+mkdir logs
+cd $PROJECT_DIR
+touch logs/gunicorn_supervisor.log
 
 # bare repo directory setup
+echo "making git repo"
 sudo mkdir -p $PINGIT_REPO
 cd $PINGIT_REPO
 git init --bare
 touch hooks/post-receive
 sudo chown pingit:webapps hooks/post-receive
-cat /vagrant/config/post-receive > hooks/post-receive
-
-
-# configure gunicorn script
-touch bin/gunicorn_start
-cat /vagrant/config/gunicorn_start >> bin/gunicorn_start
-sudo chmod u+x bin/gunicorn_start
-
-# configure supervisor
-touch /etc/supervisor/conf.d/pingit.conf
-cat /vagrant/config/supervisor.pingit.conf >> /etc/supervisor/conf.d/pingit.conf
-
-# configure nginx
-touch /etc/nginx/sites-available/pingit
-cat /vagrant/config/nginx.pingit >> /etc/nginx/sites-available/pingit
-ln -s /etc/nginx/sites-available/pingit /etc/nginx/sites-enabled/pingit
-sudo service nginx restart
+cat /vagrant/config/post-receive > hooks/post-receive && echo "configured post-receive hook"
 
 # project venv setup
+echo "setting venv"
 cd $PROJECT_DIR && virtualenv .
 source bin/activate
-pip install requirements/production.txt
+pip install requirements/production.txt && echo "requirements installed"
 # django setup
 export DJANGO_DIR=/webapps/pingit/pingit_clone
 export DJANGO_SETTINGS_MODULE=pingit_clone.settings.production
@@ -73,4 +66,22 @@ export DJANGO_SECRET_KEY="%-a9soi$l=ue_jwih)ib6yh9t4r&b6+l3xv^5^l&75i3ox8=9+"
 export BOONG_PASSWORD="9bEoYmBgnyLnFA4P2Zn0dlNUNIu6wt2KddpHKv86gggXUhjoWyDstUvNSRVHhqg"
 cd $DJANGO_DIR
 python manage.py makemigrations customers banking
-python manage.py migrate customers banking
+python manage.py migrate customers banking && echo "django has migrated"
+
+# configure gunicorn script
+echo "configure gunicorn"
+touch bin/gunicorn_start
+cat /vagrant/config/gunicorn_start >> bin/gunicorn_start && echo "configured gunicorn"
+sudo chmod u+x bin/gunicorn_start
+
+# configure supervisor
+echo "configure supervisor"
+touch /etc/supervisor/conf.d/pingit.conf
+cat /vagrant/config/supervisor.pingit.conf >> /etc/supervisor/conf.d/pingit.conf && echo "configured supervisor"
+
+# configure nginx
+echo "configuring nginx"
+touch /etc/nginx/sites-available/pingit
+cat /vagrant/config/nginx.pingit >> /etc/nginx/sites-available/pingit && echo "created config file"
+ln -s /etc/nginx/sites-available/pingit /etc/nginx/sites-enabled/pingit && echo "added config to enabled sites"
+sudo service nginx restart && echo "configured nginx"
